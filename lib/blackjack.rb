e1747b1bb913383434bf6298494e6ae306ded8d4
manon jacquin <manon.jacquin@gmail.com>
2018-10-04 16:04:19 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -69,7 +69,7 @@ def runner
   welcome 
   card_total = initial_round
   until card_total > 21
-    hit?(card_total)
+    answer = hit?(card_total)
     display_card_total()
   end 
   end_game(card_total)
